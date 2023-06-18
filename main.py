6f0488a7984747cdcb0e260460dfb725247eba1e
jadso3145 <2345jared@gmail.com>
2023-06-18 15:10:10 -0500

    python game

---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+import sys
+import pygame
+
+from settings import *
+from sprites import BG
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.display_surface = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Flappy Bird')
+        self.clock = pygame.time.Clock()
+
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_prites = pygame.sprite.Group()
+
+        BG(self.all_sprites)
+
+    def run(self):
+        last_time = time.time()
+        while True:
+
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+            self.clock.tick(FRAMERATE)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
